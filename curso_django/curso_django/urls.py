1362c1d2c8d216439244bb9aae7bda6249b73a6b
Robinson2024 <robinsonbravo97@gmail.com>
2025-02-21 14:43:49 -0300

    se avanzo a la clase 7 del curso django

--- curso_django/curso_django/urls.py
+++ curso_django/curso_django/urls.py
@@ -1,11 +1,12 @@
 
-#LAS URLS APUNTAN A LAS VISTAS, LAS VISTAS TRABAJAN CON LOS MODELOS Y NOS DEVUELVEN LAS PLANTILLAS (TEMPLATES)
+#se encarga de gestionar las rutas y qué vista ejecutar.
 from django.contrib import admin
 from django.urls import path
 from.import views
 
-urlpatterns = [
+urlpatterns =  [
     path('admin/', admin.site.urls),
     path('saludo/', views.saludo, name='saludo'),
-    path('despedida/', views.despedida, name='despedida')
+    path('despedida/', views.despedida, name='despedida'),  # Aquí falta una coma
+    path('adulto/<int:edad>/', views.adulto, name='adulto')
 ]
--- curso_django/curso_django/views.py
+++ curso_django/curso_django/views.py
@@ -1,3 +1,5 @@
+# define las funciones que procesan esas solicitudes y generan las respuestas.
+
 from django.http import HttpResponse
 
 def saludo(request):
@@ -5,3 +7,9 @@ def saludo(request):
 
 def despedida(request):
     return HttpResponse("despedida")
+
+def adulto(request, edad):
+    if edad >= 18:
+        return HttpResponse("eres mayor de edad")
+    else:
+        return HttpResponse("eres menor de edad")
