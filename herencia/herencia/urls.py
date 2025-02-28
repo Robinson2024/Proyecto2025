a59bcc920fd5dcadc9410cc9abf709aae089ced6
Robinson2024 <robinsonbravo97@gmail.com>
2025-02-28 16:05:07 -0300

    comenzando la clase 13 de enlaces e inclusion

---
+++ herencia/herencia/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [ 
+    path('admin/', admin.site.urls), # Se agrega la URL de admin
+    path('herencia/', views.herencia, name='herencia'), # Se agrega la URL de herencia
+    path('ejemplo/', views.ejemplo, name='ejemplo'), # Se agrega la URL de ejemplo
+    path('otra/', views.otra, name='otra'), # Se agrega la URL de otra
+]
+
--- plantillas/plantillas/views.py
+++ plantillas/plantillas/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render # nos permite importar la función render, o sea el codigo que se encarga de renderizar las plantillas, html, css, js, etc.
+from django.shortcuts import render  # Importa la función render para renderizar plantillas HTML
 
-def simple(request):
-    return render(request, 'simple.html', {})  # renderiza la plantilla simple.html
+def simple(request): # Renderiza la plantilla simple.html, pero no envía ningún dato adicional.
+    return render(request, 'simple.html', {})  # Renderiza la plantilla simple.html sin pasar datos
 
-def dinamico(request, name):
-    categories = ['code' , 'design', 'marketing']  # crea una lista con las categorias
-    context = {'name': name, 'categories': categories}	# crea un diccionario con el nombre que se le pasa por la url
-    return render(request, 'dinamico.html', context)  # renderiza la plantilla dinamico.html con el contexto creado
-
+def dinamico(request, name): #Recibe un parámetro name desde la URL
+    categories = ['code', 'design', 'marketing', 'bussines']  # Lista de categorías
+    context = {'name': name, 'categories': categories}  # Crea un diccionario context que contiene: 'name': El nombre que se recibe de la URL.'categories': La lista de categorías.
+    return render(request, 'dinamico.html', context)  # Renderiza la plantilla dinamico.html con el contexto
